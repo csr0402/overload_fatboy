de5ea8137c29c5ccb687b260333e98d0d7c08c4f
csr0402 <csr0402@users.noreply.github.com>
2026-06-02 23:44:19 +0800

    feat(summary): implement dynamic gauge charts for affection and knowledge rates

--- attractions.js
+++ attractions.js
@@ -1,33 +1,73 @@
 window.attractionsData = [
   {
-    "name": "溪湖羊肉爐",
+    "name": "阿秀羊肉爐",
     "region": "溪湖鎮",
     "desc": "全台聞名的在地美食，中藥湯頭搭配鮮嫩羊肉，是冬日溫補首選！",
-    "link": "xihu-attractions.html",
-    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    "times": ["Morning", "Afternoon", "Evening"]
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.084552113781!2d120.48226057602737!3d23.957450080794807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694983aae307fd%3A0x82185585499412b6!2z5rqq5rmW6Zi_56eA576K6IKJ54iQ!5e0!3m2!1szh-TW!2stw!4v1780412939835!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Afternoon", "Evening"]
   },
   {
     "name": "溪湖糖廠觀光小火車",
     "region": "溪湖鎮",
     "desc": "搭乘懷舊的五分車穿梭於田野之間，感受日治時期遺留下來的糖業風情。",
-    "link": "xihu-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14584.335477395107!2d120.47453576420696!3d23.957474227194307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469497febe4deb5%3A0x84212ba30c581800!2z5rqq5rmW57OW5bug!5e0!3m2!1szh-TW!2stw!4v1780413195587!5m2!1szh-TW!2stw",
     "days": ["Saturday", "Sunday"],
     "times": ["Morning", "Afternoon"]
   },
   {
-    "name": "葡萄產業文化園區",
+    "name": "巫家捏麵館",
     "region": "溪湖鎮",
-    "desc": "產地直採的巨峰葡萄，果肉飽滿多汁，適合全家大小一起體驗採果樂趣。",
-    "link": "xihu-attractions.html",
+    "desc": "充滿童趣的手工藝體驗，透過捏麵人的創作，感受簡單的幸福與創意。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.664951961551!2d120.47545817602762!3d23.972285180220613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694975a4b819a1%3A0xfc3f168afb368baa!2z5ber5a625o2P6bq16aSo!5e0!3m2!1szh-TW!2stw!4v1780413269300!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon"]
+  },
+  {
+    "name": "中央公園",
+    "region": "溪湖鎮",
+    "desc": "綠意盎然的城市綠洲，是市民散步、慢跑及親子休閒的絕佳去處。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.325624388451!2d120.47691977602717!3d23.9489229811247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694b8838236869%3A0xaea88e9d84ba2951!2z5rqq5rmW5Lit5aSu5YWs5ZyS!5e0!3m2!1szh-TW!2stw!4v1780414013197!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Morning", "Afternoon"]
+  },
+  {
+    "name": "榮記當歸鴨",
+    "region": "溪湖鎮",
+    "desc": "溪湖鎮排隊名店，古早味當歸鴨肉湯清甜不油膩，搭配鴨肉飯享用，是絕不能錯過的道地小吃。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.907276254789!2d120.47659947602746!3d23.963718780552036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694977a9318a87%3A0x631eb1d2ee5067d5!2z5qau6KiY55W25q246bSo!5e0!3m2!1szh-TW!2stw!4v1780414117310!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Afternoon", "Evening"]
+  },
+  {
+    "name": "阿讚豬腳",
+    "region": "溪湖鎮",
+    "desc": "溪湖鎮老店，獨特的滷汁讓豬腳入味而不死鹹，搭配獨家醬料，是許多在地人指定的宵夜小吃。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.079230542643!2d120.48080092602734!3d23.957638280787364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694977abb5c4f1%3A0x340f5bde232e8b27!2z5rqq5rmW6Zi_6K6a6LGs6IWz54iM6IKJ6aOv!5e0!3m2!1szh-TW!2stw!4v1780414216484!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Afternoon", "Evening"]
+  },
+  {
+    "name": "溪湖福安宮",
+    "region": "溪湖鎮",
+    "desc": "歷史悠久的信仰中心，香火鼎盛，吸引無數信眾前來祈求平安與好運。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.901516549003!2d120.47444108644486!3d23.96392242518923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346949001cd5aa4b%3A0x4bbb1a7aa95c7f87!2z5rqq5rmW56aP5a6J5a6u!5e0!3m2!1szh-TW!2stw!4v1780414296770!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Morning", "Afternoon", "Evening"]
+  },
+  {
+    "name": "羊仔頭",
+    "region": "溪湖鎮",
+    "desc": "全台聞名的在地美食，中藥湯頭搭配鮮嫩羊肉，是冬日溫補首選！",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3646.082909273754!2d120.48028177602751!3d23.9575081807925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34694913e0b5d8eb%3A0x14cbbd0a234345f7!2z5qWK5LuU6aCt576K6IKJ54iQ!5e0!3m2!1szh-TW!2stw!4v1780414409974!5m2!1szh-TW!2stw",
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    "times": ["Afternoon", "Evening"]
   },
   {
     "name": "統一夢時代購物中心",
     "region": "前鎮區",
     "desc": "南台灣超大型購物商城，頂樓更有著名的「高雄之眼」摩天輪，逛街約會一次滿足。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.578290558971!2d120.3031791759994!3d22.59487053215242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0372ebc9d327%3A0x92317d11de68daf9!2z5aSi5pmC5Luj6LO854mp5Lit5b-DL-e1seS4gOaZguS7o-eZvuiyqOmrmOmbhOW6lw!5e0!3m2!1szh-TW!2stw!4v1780413364372!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
@@ -35,7 +75,7 @@ window.attractionsData = [
     "name": "珍芳烏魚子見學工廠",
     "region": "前鎮區",
     "desc": "深入了解台灣烏魚子製作工藝的觀光工廠，體驗傳統海味的製作與試吃。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.909044262549!2d120.31020287599928!3d22.582504932605904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0310ebfd4717%3A0x6f51b0dfd404e9e9!2z54-N6Iqz54OP6a2a5a2Q6KaL5a245bel5bug772c5Y-D6KeA5o6h6aCQ57SE5Yi2!5e0!3m2!1szh-TW!2stw!4v1780413406486!5m2!1szh-TW!2stw",
     "days": ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon"]
   },
@@ -43,15 +83,15 @@ window.attractionsData = [
     "name": "光華觀光夜市",
     "region": "前鎮區",
     "desc": "在地人最愛的宵夜一條街，從木瓜牛奶到各式傳統小吃應有進有。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3682.953146983722!2d120.31525317599994!3d22.61822473129521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0498d7d46075%3A0x4f0a86b6c9e8426e!2z5YWJ6I-v6KeA5YWJ5aSc5biC!5e0!3m2!1szh-TW!2stw!4v1780413444642!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Evening"]
   },
   {
-    "name": "前鎮之星自行車橋",
+    "name": "前鎮之星",
     "region": "前鎮區",
     "desc": "造型優美如同白色藤蔓的空中自行車道，搭配輕軌列車駛過，是攝影打卡熱點。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.5684955414595!2d120.31270487599936!3d22.595236632139002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e033ea5e05d7f%3A0xee3e55539fc00f0e!2z5YmN6Y6u5LmL5pif!5e0!3m2!1szh-TW!2stw!4v1780413484241!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
@@ -59,7 +99,7 @@ window.attractionsData = [
     "name": "高雄市立圖書館總館",
     "region": "前鎮區",
     "desc": "是高雄市的文化地標之一，建築設計新穎，內部空間寬敞明亮，提供豐富的藏書資源與學習空間，也是親子共遊的好去處。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.161362358123!2d120.29922397599985!3d22.610448731580735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0c995f8dcfb3%3A0x62d29ce2a4f9e568!2z6auY6ZuE5biC56uL5ZyW5pu46aSo57i96aSo!5e0!3m2!1szh-TW!2stw!4v1780413550284!5m2!1szh-TW!2stw",
     "days": ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
@@ -67,7 +107,7 @@ window.attractionsData = [
     "name": "時代棧",
     "region": "前鎮區",
     "desc": "位在高雄捷運紅線獅甲站旁，是一處結合藝文、設計與共享空間的複合式場所。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.5419591443338!2d120.30475917599944!3d22.596228432102578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e036df3bcd017%3A0x9accfd5f8a921c9a!2zODA26auY6ZuE5biC5YmN6Y6u5Y2A5pmC5Luj5aSn6YGT!5e0!3m2!1szh-TW!2stw!4v1780413761570!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
@@ -75,7 +115,7 @@ window.attractionsData = [
     "name": "亞灣成功商圈",
     "region": "前鎮區",
     "desc": "亞灣成功商圈緊鄰高雄港，過去曾是重要的港口貿易集散地，近年來隨著亞洲新灣區的開發，轉型為兼具商業、觀光與休閒機能的濱海商圈。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14734.165223627437!2d120.29703436375742!3d22.596252846385163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e033d9a2af75d%3A0x671d44309c4e789a!2z5Lqe54Gj5paw5Ym15ZySIFN0YXJ0dXAgVGVycmFjZSBLYW9oc2l1bmc!5e0!3m2!1szh-TW!2stw!4v1780413810326!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
@@ -83,15 +123,15 @@ window.attractionsData = [
     "name": "大魯閣草衙道 SKM Park",
     "region": "前鎮區",
     "desc": "大魯閣草衙道是一座結合購物中心、休閒娛樂與棒壘球打擊場的複合式商場，提供多樣化的休閒選擇。",
-    "link": "qianzhen-attractions.html",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.9065199117185!2d120.32676277599923!3d22.582599332602513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e0480a9ebd505%3A0x932e7944827ed71!2zU0tNIFBhcmsgT3V0bGV0cyDpq5jpm4TojYnooZk!5e0!3m2!1szh-TW!2stw!4v1780413847253!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   },
   {
-    "name": "前鎮河親水自行車道",
+    "name": "親水公園",
     "region": "前鎮區",
-    "desc": "前鎮河親水自行車道沿著前鎮河兩岸而建，提供舒適的自行車道，是休閒運動的好去處。",
-    "link": "qianzhen-attractions.html",
+    "desc": "親水公園沿著前鎮河兩岸而建，提供舒適的自行車道，是休閒運動的好去處。",
+    "link": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.917566546825!2d120.3284236759992!3d22.582186232617577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e030074ee3337%3A0xec3bc688b8e496f7!2z6Kaq5rC05YWs5ZyS!5e0!3m2!1szh-TW!2stw!4v1780413913373!5m2!1szh-TW!2stw",
     "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
     "times": ["Morning", "Afternoon", "Evening"]
   }
